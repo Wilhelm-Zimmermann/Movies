decaf2722398c2beefab88b523ec9526538738b0
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-05 15:22:27 -0300

    added Entity Base

---
+++ Movies.Domain/Entities/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Domain.Entities
+{
+    public abstract class EntityBase : IEquatable<EntityBase>
+    {
+        public Guid Id { get; private set; }
+
+        public EntityBase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public bool Equals(EntityBase? other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
