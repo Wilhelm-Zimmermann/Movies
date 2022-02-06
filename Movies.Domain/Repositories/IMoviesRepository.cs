3bdec14e44a4db07685a658deda3a1fe6d32bbcc
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-06 09:20:24 -0300

    Created movies Repository

---
+++ Movies.Domain/Repositories/IMoviesRepository.cs
@@ -0,0 +1,19 @@
+﻿using Movies.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Domain.Repositories
+{
+    public interface IMoviesRepository
+    {
+        Task CreateMovie(Movie movie);
+        Task UpdateMovie(Movie movie);
+        Task DeleteMovie(Movie movie);
+        Task<IEnumerable<Movie>> GetMovies();
+        Task<Movie> GetMovieById(Guid id);
+        Task<Movie> GetMoviesPerCategory(string category);
+    }
+}
