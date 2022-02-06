095a412e3cd9756ac84dc169121f155b1ed9a3f3
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-06 09:24:37 -0300

    Creted movies queries

---
+++ Movies.Domain/Queries/MoviesQueries.cs
@@ -0,0 +1,23 @@
+﻿using Movies.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Domain.Queries
+{
+    public class MoviesQueries
+    {
+        public Expression<Func<Movie, bool>> GetMovieById(Guid id)
+        {
+            return x => x.Id == id;
+        }
+
+        public Expression<Func<Movie,bool>> GetMoviesPerCategory(string category)
+        {
+            return x => x.Category == category;
+        }
+    }
+}
