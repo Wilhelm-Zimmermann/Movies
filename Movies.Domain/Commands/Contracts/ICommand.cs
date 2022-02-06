c4f2ef65e9363ad7adae3864ce432d9e4b423b8c
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-06 09:40:40 -0300

    Created Generic command result, this is always returns as response

--- Movies.Domain/Commands/Contracts/ICommand.cs
+++ Movies.Domain/Commands/Contracts/ICommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Movies.Domain.Commands.Contracts
+﻿namespace Movies.Domain.Commands.Contracts
 {
     public interface ICommand
     {
