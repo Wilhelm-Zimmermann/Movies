687257257d6766c1fa4406cc7046d2cc9f5342ae
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-12 16:54:50 -0300

    created category entity

---
+++ Movies.Domain/Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public string Title { get; private set; }
+
+        public Category(string title)
+        {
+            Title = title;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+    }
+}
