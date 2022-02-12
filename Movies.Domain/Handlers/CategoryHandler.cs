e032c4240cfc74cfd7f39a422d1007b7ab5b3798
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-12 16:55:24 -0300

    created category handler to handle category commands

---
+++ Movies.Domain/Handlers/CategoryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Movies.Domain.Commands;
+using Movies.Domain.Commands.Contracts;
+using Movies.Domain.Entities;
+using Movies.Domain.Handlers.Contracts;
+using Movies.Domain.Repositories;
+
+namespace Movies.Domain.Handlers
+{
+    public class CategoryHandler : IHandler<CreateCategoryCommand>
+    {
+        private readonly ICategoriesRepository _categoriesRepository;
+
+        public CategoryHandler(ICategoriesRepository categoriesRepository)
+        {
+            _categoriesRepository = categoriesRepository;
+        }
+
+        public async Task<ICommandResult> Handle(CreateCategoryCommand command)
+        {
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult("Invalid Category", false, command.Notifications);
+
+            var category = new Category(command.Title);
+
+            await _categoriesRepository.CreateCategory(category);
+
+            return new GenericCommandResult("Category created successfully", true, category); 
+        }
+    }
+}
