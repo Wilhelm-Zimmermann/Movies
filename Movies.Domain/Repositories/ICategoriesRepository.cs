1186960a29ab9040c021e0c815882afaca5f7115
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-12 16:55:39 -0300

    created CategoriesRepository interface

---
+++ Movies.Domain/Repositories/ICategoriesRepository.cs
@@ -0,0 +1,9 @@
+﻿using Movies.Domain.Entities;
+
+namespace Movies.Domain.Repositories
+{
+    public interface ICategoriesRepository
+    {
+        Task CreateCategory(Category category);
+    }
+}
