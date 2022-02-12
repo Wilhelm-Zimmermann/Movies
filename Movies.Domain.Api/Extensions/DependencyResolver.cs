924bff86ab2f1cb44e56c24c2c2f7f6e3b3b0d90
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-12 12:05:03 -0300

    this file resolve all dependencies, like repositories, handlers, and database

---
+++ Movies.Domain.Api/Extensions/DependencyResolver.cs
@@ -0,0 +1,19 @@
+﻿using Movies.Domain.Handlers;
+using Microsoft.EntityFrameworkCore;
+using Movies.Domain.Handlers.Contracts;
+using Movies.Domain.Infra.Contexts;
+using Movies.Domain.Repositories;
+using Movies.Domain.Infra.Repositories;
+
+namespace Movies.Domain.Api.Extensions
+{
+    public static class DependencyResolver
+    {
+        public static void ResolveDependencies(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<MovieContext>(opt => opt.UseInMemoryDatabase("Database"));
+            builder.Services.AddScoped<MovieHandler, MovieHandler>();
+            builder.Services.AddScoped<IMoviesRepository, MoviesRepository>();
+        }
+    }
+}
