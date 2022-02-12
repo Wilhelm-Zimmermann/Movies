4153bd4a7c48d128f08d2406300a58ef17b642c8
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-12 12:04:31 -0300

    route change

--- Movies.Domain.Api/Controllers/MovieController.cs
+++ Movies.Domain.Api/Controllers/MovieController.cs
@@ -1,18 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Movies.Domain.Commands;
+using Movies.Domain.Entities;
 using Movies.Domain.Handlers;
+using Movies.Domain.Repositories;
 
 namespace Movies.Domain.Api.Controllers
 {
     [ApiController]
-    [Route("movies/new")]
+    [Route("movies")]
     public class MovieController : ControllerBase
     {
         [HttpPost]
-        public async Task<GenericCommandResult> CreateMovie([FromBody] CreateMovieCommand command, [FromServices] MovieHandler handler)
+        [Route("new")]
+        public async Task<GenericCommandResult> CreateMovie([FromForm] CreateMovieCommand command, [FromServices] MovieHandler handler)
         {
             var handle = (GenericCommandResult) await handler.Handle(command);
             return handle;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Movie>> GetMovies([FromServices] IMoviesRepository repository)
+        {
+            return await repository.GetMovies();
         }
     }
 }
