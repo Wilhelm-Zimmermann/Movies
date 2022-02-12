5c128812852ef6e09e93e9a959049ffea2491298
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-12 12:02:48 -0300

    Create Movie, and Get all movies

---
+++ Movies.Domain.Api/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Domain.Commands;
+using Movies.Domain.Handlers;
+
+namespace Movies.Domain.Api.Controllers
+{
+    [ApiController]
+    [Route("movies/new")]
+    public class MovieController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<GenericCommandResult> CreateMovie([FromBody] CreateMovieCommand command, [FromServices] MovieHandler handler)
+        {
+            var handle = (GenericCommandResult) await handler.Handle(command);
+            return handle;
+        }
+    }
+}
