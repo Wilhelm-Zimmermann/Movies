bbacb00d3912be58a0634435429c322281f51128
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2022-02-12 16:55:08 -0300

    created a command to create categories

---
+++ Movies.Domain/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,32 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Movies.Domain.Commands.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Domain.Commands
+{
+    public class CreateCategoryCommand : Notifiable, ICommand
+    {
+        public string Title { get; set; }
+
+        public CreateCategoryCommand()
+        {
+
+        }
+        public CreateCategoryCommand(string title)
+        {
+            Title = title;
+        }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(Title, 3, "Category.Title", "Category min len is 3");
+        }
+    }
+}
